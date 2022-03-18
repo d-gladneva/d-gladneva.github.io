2a2757e175087210741f9c8c616238e2bda29cfb
Darya Gladneva <davydova_di@mail.ru>
2022-03-18 17:42:40 +0300

    feat: app-pgames

--- my-app-games/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
-
-rerenderEntireTree(state);
